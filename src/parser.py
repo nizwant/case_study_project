5c80c8f884d12e38f989d1c624eccdbd80da99ba
nizwant <mateusznizwantowski@gmail.com>
2023-03-12 16:51:29 +0100

    parser to files with tsp problem

---
+++ src/parser.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+name = "ftv38.atsp"
+
+
+def parse(file_path):
+    with open(file_path, "r") as f:
+        # read the file line by line
+        data = f.read().split()
+        size = int(data[data.index("DIMENSION:") + 1])
+        martix = np.array(
+            data[data.index("EDGE_WEIGHT_SECTION") + 1 : -1], dtype=np.int32
+        )
+    return martix.reshape(size, size)
+
+
+print(parse(name))
---
+++ src/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print("hello world")
+print(np.array([2, 1, 3, 4]))
