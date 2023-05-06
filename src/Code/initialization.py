e76309b681c4974ff633415b62e8674b687ef069
nizwant <mateusznizwantowski@gmail.com>
2023-05-06 18:03:28 +0200

    nearest neighbour

--- src/Code/initialization.py
+++ src/Code/initialization.py
@@ -22,13 +22,41 @@ def initialize_transition_function_type(n: int, probability_of_shuffle: float) -
     return [random.random() < probability_of_shuffle for _ in range(n)]
 
 
-def initialize_initial_solution(n: int, probability_of_heuristic: float) -> list:
+def initialize_initial_solution(
+    n: int, distance_matrix: float, probability_of_heuristic: float
+) -> list:
     """
     Returns a list of n solutions, where
     q[i] has probability_of_heuristic chance of being a heuristic initial solution.
     heuristic used here is nearest neighbor.
     """
-    # zapisac raz rozwiazanie nearest neighbor i potem je kopiowac
-    # stworzyc funkcje nearest neighbor
-    # stworzyc funkcje random initial solution
+    nearest_neighbor_solution = nearest_neighbor_initial_solution(distance_matrix)
+
     return [random.random() < probability_of_heuristic for _ in range(n)]
+
+
+def nearest_neighbor_initial_solution(distance_matrix: float) -> list:
+    """
+    Finds a suboptimal solution to the asymmetric Traveling Salesman Problem (TSP)
+    It is irrelevant what values are on the diagonal of the matrix
+
+    :return: list:
+            A list of integers representing the order in which cities should be visited to obtain a suboptimal
+            solution to the TSP. The first city in the path is always city 0.
+    """
+    size = distance_matrix.shape[0]
+    unvisited = set(range(1, size))
+    path = [0]
+    current_city = 0
+    while unvisited:
+        nearest_neighbor = min(
+            unvisited, key=lambda city: distance_matrix[current_city][city]
+        )
+        unvisited.remove(nearest_neighbor)
+        path.append(nearest_neighbor)
+        current_city = nearest_neighbor
+    return path
+
+
+def random_initial_solution():
+    pass
