2462393cd15d330b7b026e8cd16a7ca0c8cd6643
szuvarska <01171269@pw.edu.pl>
2023-05-07 20:08:41 +0200

    Add transition_function_swap and acceptance functions

--- src/Code/initialization.py
+++ src/Code/initialization.py
@@ -4,13 +4,14 @@ from random import shuffle
 
 
 def initialize_temperatures(
-    n: int, min: float, max: float, a: float = 1, b: float = 1
+        n: int, min: float, max: float, a: float = 1, b: float = 1
 ) -> list:
     """
     Returns a list of n temperatures between min and max,
     with a and b as parameters for the beta distribution.
     if a = b = 1, the temperatures are uniformly distributed between min and max.
     """
+    # modification to consider: adding distribution type as parameter ~Marta
     return beta(a, b, n) * (max - min) + min
 
 
@@ -24,7 +25,7 @@ def initialize_transition_function_types(n: int, probability_of_shuffle: float) 
 
 
 def initialize_initial_solutions(
-    n: int, distance_matrix: list[list[float]], probability_of_heuristic: float
+        n: int, distance_matrix: list[list[float]], probability_of_heuristic: float
 ) -> list:
     """
     Returns a list of n solutions, where
@@ -40,6 +41,10 @@ def initialize_initial_solutions(
     for i in range(n):
         if random() < probability_of_heuristic:
             initial_solution[i] = nearest_neighbor_solution
+            # modification to consider: instead of calculating nearest neigbour solution only once
+            # before entering the loop, we may calculate it here (then we do it only when we need it)
+            # and give them opportunity to be different by e.g. treating starting city as parameter
+            # ~ Marta
         else:
             initial_solution[i] = random_initial_solution(distance_matrix)
     return initial_solution
@@ -83,14 +88,14 @@ def random_initial_solution(distance_matrix: list[list[float]]) -> list:
 
 
 def initialization(
-    distance_matrix: list[list[float]],
-    n: int,
-    min_temperature: float,
-    max_temperature: float,
-    probability_of_shuffle: float,
-    probability_of_heuristic: float,
-    a: float,
-    b: float,
+        distance_matrix: list[list[float]],
+        n: int,
+        min_temperature: float,
+        max_temperature: float,
+        probability_of_shuffle: float,
+        probability_of_heuristic: float,
+        a: float,
+        b: float,
 ) -> tuple:
     """
     Returns a tuple of lists, where
