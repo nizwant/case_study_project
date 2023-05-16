db3032dd7df10096738e9aec10ee9e7b85bc0049
szuvarska <01171269@pw.edu.pl>
2023-05-16 23:34:11 +0200

    Update parameters

--- src/Code/main.py
+++ src/Code/main.py
@@ -3,7 +3,8 @@ import time
 from pt_sa import pt_sa
 from src.Code.read_input import read_input
 from src.Parameters.creating_df_for_parameter import generate_dataframe
-import pandas as pd
+
+
 def main():
     # read from file
     # distance_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
@@ -12,25 +13,26 @@ def main():
 
     # random parameters, do not take them as point of reference
     parameters = {
-        "n": 10,
+        "n": 20,  # 10
         "min_temperature": 0.1,
-        "max_temperature": 100,
-        "probability_of_shuffle": 0.5,
-        "probability_of_heuristic": 0.5,
-        "a": 1,
-        "b": 1,
+        "max_temperature": 50,
+        "probability_of_shuffle": 0.1,  # 0.5
+        "probability_of_heuristic": 0.7,  # 0.5
+        "a": 1,  # 1
+        "b": 1,  # 1
         "duration_of_execution_in_seconds": 5,  # 60 * 4,
-        "k": 10,
-        "max_length_percent_of_cycle": 0.2,  # max 0.3 more will result in bugs
+        "k": 20,  # 10
+        "max_length_percent_of_cycle": 0.3,  # max 0.3 more will result in bugs
         "swap_states_probability": 0.1,
         "closeness": 1.5,
-        "cooling_rate": 0.1  # probably too low; actually suggested above 0.9
+        "cooling_rate": 0.95  # 0.1 probably too low; actually suggested above 0.9
     }
 
     solution, solution_length = pt_sa(distance_matrix, **parameters)
     print(f"Solution: {solution}\nSolution length: {solution_length}")
 
-### parameters_test
+    ### parameters_test
+    # TODO: should it be here?
     values = [0.1,0.2,0.3,0.4,0.5]
     df = generate_dataframe(parameters, values, 'min_temperature', distance_matrix)
     print(df.head())
