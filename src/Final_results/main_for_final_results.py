ada0b925100bfad3709b00c81192408138119a17
MagdalenaJeczen <01171155@pw.edu.pl>
2023-06-03 16:26:11 +0200

    final results update2

--- src/Final_results/main_for_final_results.py
+++ src/Final_results/main_for_final_results.py
@@ -6,6 +6,8 @@ from src.Parameters.problems import problems
 import pandas as pd
 from pt_sa_for_results import pt_sa_for_results
 
+from src.Code.pt_sa import pt_sa
+
 def set_parameters(exec_time: float) -> dict:
     parameters = {
         "n": 20,  # 10
@@ -15,7 +17,7 @@ def set_parameters(exec_time: float) -> dict:
         "probability_of_heuristic": 0.7,  # 0.5
         "a": 1,  # 1
         "b": 1,  # 1
-        "duration_of_execution_in_seconds": exec_time - 0.1,  # 60 * 4,
+        "duration_of_execution_in_seconds": exec_time - 30,  # 60 * 4,
         "k": 20,  # 10
         "max_length_percent_of_cycle": 0.3,  # max 0.3 more will result in bugs
         "swap_states_probability": 0.1,
@@ -27,7 +29,7 @@ def set_parameters(exec_time: float) -> dict:
 def run_algorithm_for_results(problem_name: str) -> pd.DataFrame:
     distance_matrix = problems[problem_name]
     N = len(distance_matrix)
-    exec_time = 60 * 5
+    exec_time = 300
 
     if N < 300:
         num_of_runs = int(exec_time / 30)
@@ -51,29 +53,49 @@ def df_time_creating(filename_prefix, num_iterations):
         csv_filename = f"{filename_prefix}_{i+1}.csv"
         df.to_csv(csv_filename, index=False)
 
+def iterate_over_all_problems_2():
+    data = []
+
+    for problem_name, distance_matrix in problems.items():
+        best_solution, best_solution_length = run_algorithm(problem_name)
+        optimal_solution_length = best_known_solution[problem_name]
+
+        data.append({
+            'our_solution_length': best_solution_length,
+            'optimal_solution_length': optimal_solution_length
+        })
+
+    df = pd.DataFrame(data)
+    return df
+
+def run_algorithm(problem_name: str):
+    distance_matrix = problems[problem_name]
+    N = len(distance_matrix)
+    exec_time = 60 * 5
+    if N < 300:
+        num_of_runs = int(exec_time / 30)
+        parameters = set_parameters(30)
+    else:
+        num_of_runs = int(exec_time / 150)
+        parameters = set_parameters(150)
+    best_solution = [None for _ in range(len(distance_matrix))]
+    best_solution_length = float("inf")
+    for _ in range(num_of_runs):
+        solution, solution_length = pt_sa(distance_matrix, **parameters)
+        if solution_length < best_solution_length:
+            best_solution, best_solution_length = solution, solution_length
+    return best_solution, best_solution_length
+
 def main():
-    ### results:
-    #df = pd.DataFrame(columns=['best_solution_length', 'time'])
-    #df = run_algorithm_for_results("br17")
-    #solution, solution_length = pt_sa_for_results(problems["br17"], **set_parameters(10), df=df)
-    #print(f"Last best solution: {solution}\nBest solution length: {solution_length}")
-    #print(df)
-    #df.to_csv("output_second_try.csv", index=False)
-
-    ## for small
-    #df = run_algorithm_for_results("ftv35")
-    #print(df)
-    #df.to_csv("sol_time_small.csv", index=False)
-
-    #for medium
-    #df = run_algorithm_for_results("ftv64")
-    #print(df)
-    #df.to_csv("sol_time_medium.csv", index=False)
-
-    # for big
-    # print(df)
-    # df.to_csv("sol_time_big.csv", index=False)
-    df_time_creating('ftv35',15)
+    #df_time_creating('ftv35',15)
+    #df_time_creating('ft70', 15)
+    #df = iterate_over_all_problems_2()
+    #df.to_csv("all_problems_results_2.csv", index=False)
+    #df = iterate_over_all_problems_2()
+    #df.to_csv("all_problems_results_3.csv", index=False)
+    #df = iterate_over_all_problems_2()
+    #df.to_csv("all_problems_results_4.csv", index=False)
+    df_time_creating('rbg358', 1)
 
 
 if __name__ == "__main__":
