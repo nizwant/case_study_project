7534586777ab894f08cade8d0c81baa95935aabb
nizwant <mateusznizwantowski@gmail.com>
2023-06-04 18:23:41 +0200

    explaining main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
 import time
-from Code.run_algorithm import run_for_one_problem, iterate_over_all_problems, iterate_over_all_problems_with_time
+from Code.run_algorithm import (
+    run_for_one_problem,
+    iterate_over_all_problems,
+    iterate_over_all_problems_with_time,
+)
 
 
 def main():
+    # uncomment one of the following lines to run the algorithm
+
+    # plain iteration over all problems 5 minutes each, recreation of the version from The Race
+    iterate_over_all_problems()
+
+    # solve one problem given by its name
     # run_for_one_problem("rbg403")
-    iterate_over_all_problems_with_time(30 * 60)
+
+    # iterate over all problems with time limit in seconds
+    # iterate_over_all_problems_with_time(30 * 60)
 
 
 if __name__ == "__main__":
